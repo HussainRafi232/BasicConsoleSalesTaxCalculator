e75563c4f02206567e71fd97bca5f974afa354a6
Hussain Rafi <78901286+HussainRafi232@users.noreply.github.com>
2021-02-11 14:52:13 +0500

    Add files via upload

---
+++ SalesTaxCalculator/Process.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace SalesTaxCalculator
+{
+    class Process
+    {
+        //Formulas
+        //TST = Item Cost x Sales Tax Rate / 100
+        //TC = Item Cost + Total Sales Tax
+        //STR = Total Sales Tax / Item Cost x 100
+        
+
+        public double itemcost; // Double variable for Item Cost
+        public double totalsalestax; // Double variable for Total Sales Tax
+        public double salestaxrate; // Double variable for Sales Tax Rate
+        public double totalcost; // Double variable for Total Cost
+        
+        public void ClearScreen() // Method to Clear Screen 
+        {
+            Console.Clear();
+            Console.ReadKey();
+        }
+
+        // Method for Error Message if one of the user input
+        // is zero or less than zero
+        public void ErrorMessage() 
+        {
+            Console.WriteLine("Error! Amount is 0. Try again. Press any key to continue");
+        }
+        public void TotalSalesTax()
+        {
+            Console.WriteLine("Enter Item Cost:");
+            itemcost = Double.Parse(Console.ReadLine()); 
+            if(itemcost == 0 || itemcost < 0) { ErrorMessage(); ClearScreen(); TotalSalesTax(); }
+            Console.WriteLine("Enter Sales Tax Rate:");
+            salestaxrate = Double.Parse(Console.ReadLine()) / 100;
+            if (salestaxrate == 0 || salestaxrate < 0) { ErrorMessage(); ClearScreen(); TotalSalesTax(); }
+            totalsalestax = itemcost * salestaxrate;
+            Console.WriteLine("Your total sales tax is "+totalsalestax);
+            
+        }
+        public void TotalCost()
+        {
+            Console.WriteLine("Enter Item Cost:");
+            itemcost = Double.Parse(Console.ReadLine());
+            if (itemcost == 0 || itemcost < 0) { ErrorMessage(); ClearScreen(); TotalCost(); }
+            Console.WriteLine("Enter Total Sales Tax:");
+            totalsalestax = Double.Parse(Console.ReadLine());
+            if (totalsalestax == 0 || totalsalestax < 0) { ErrorMessage(); ClearScreen(); TotalCost(); }
+            totalcost = itemcost + totalsalestax;
+            Console.WriteLine("Your total cost is " + totalcost);
+            
+        }
+
+        public void SalesTaxRate()
+        {
+            Console.WriteLine("Enter Total Sales Tax:");
+            totalsalestax = Double.Parse(Console.ReadLine());
+            if (totalsalestax == 0 || totalsalestax < 0) { ErrorMessage(); ClearScreen(); SalesTaxRate(); }
+            Console.WriteLine("Enter Item Cost:");
+            itemcost = Double.Parse(Console.ReadLine());
+            if (itemcost == 0 || itemcost < 0) { ErrorMessage(); ClearScreen(); SalesTaxRate(); }
+            salestaxrate = totalsalestax / itemcost * 100;
+            Console.WriteLine("Your sales tax rate is " + salestaxrate + "%");
+            
+        }
+
+    }
+}
---
+++ SalesTaxCalculator/Program.cs
@@ -0,0 +1,141 @@
+﻿using System;
+
+namespace SalesTaxCalculator
+{
+    class Program 
+    {
+
+        Process pobj = new Process();
+        
+        //Formulas
+        //TST = Item Cost x Sales Tax Rate / 100
+        //TC = Item Cost + Total Sales Tax
+        //STR = Total Sales Tax / Item Cost x 100
+
+        /* Total Sales Tax method for containing the 
+          actual function from the Process.cs Class
+            */
+        public void TST()  
+        {
+            pobj.TotalSalesTax();
+        }
+        /* Total Cost method for containing the 
+          actual function from the Process.cs Class
+            */
+        public void TC()
+        {
+            pobj.TotalCost();
+        }
+        /* Sales Tax Rate method for containing the 
+          actual function from the Process.cs Class
+            */
+        public void STR()
+        {
+            pobj.SalesTaxRate();
+        }
+       
+        /*Method for returning to Main after user gets
+          the calculated output for the Tax Rate/Cost/etc on
+          the press of a key.
+            */
+        public void Return()
+        {
+            String option = Console.ReadLine();
+            if(option == "y" || option == "Y")
+            {
+                Console.Clear();
+                Main();
+            }
+            else if(option == "n" || option == "N")
+            {
+                System.Environment.Exit(0);
+            }
+            else
+            {
+                Console.WriteLine("Wrong option. Try again");
+                Return();
+            }    
+            
+        }
+        // Method for Display message for Return
+        public void ReturnMessage()
+        {
+            String message = "Continue? Y/n";
+            Console.WriteLine(message);
+        }
+        // Method for Instructions on how to calculate
+        public void Instructions()
+        {
+
+            Console.WriteLine("Sales Tax Calculator - HUSSAIN RAFI");
+            Console.WriteLine("Enter one of the numbers shown on the screen to calculate/or to get help");
+            Console.WriteLine("1) Calculate Total Sales Tax 2) Calculate Total Cost");
+            Console.WriteLine("3) Calculate Sales Tax Rate 4) Help 5) Exit");
+        }
+        // Method to explain the end-user on how these calculations work through a formula
+        public void Help()
+        {
+            
+            Console.WriteLine("These are the formulas for calculating sales tax on a certain product");
+            Console.WriteLine("Total Sales Tax = Item Cost * Sales Tax Rate / 100");
+            Console.WriteLine("Total Cost = Item Cost + Total Sales Tax");
+            Console.WriteLine("Sales Tax Rate = Total Sales Tax / Item Cost * 100");
+            
+        }
+        public void SelectOption()
+        {
+            Program pr = new Program();
+            int option = Int32.Parse(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    pr.TST(); // Total Sales Tax
+                    pr.ReturnMessage();
+                    pr.Return();
+                    break;
+
+                case 2:
+                    pr.TC(); // Total Cost
+                    pr.ReturnMessage();
+                    pr.Return();
+                    break;
+
+                case 3:
+                    pr.STR(); // Sales Tax Rate
+                    pr.ReturnMessage();
+                    pr.Return();
+                    break;
+
+                case 4:
+                    pr.Help(); // Help
+                    pr.ReturnMessage();
+                    pr.Return();
+                    break;
+
+                case 5: // Exit
+                    System.Environment.Exit(0);
+                    break;
+
+                default:
+                    Console.WriteLine("Error! Wrong option. Please try again");
+                    Console.ReadKey();
+                    Console.Clear();
+                    Main();
+                    break;
+
+            }
+        }
+        public static void Main()
+        {
+            Console.Title = "Sales Tax Calculator";
+            Program pr = new Program();
+            pr.Instructions();
+            pr.SelectOption();
+
+
+            Console.ReadKey();
+        }
+
+        
+    }
+}
